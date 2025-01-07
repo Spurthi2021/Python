e6c412c81340aa5eae51a4a40c62226adda051b1
Spurthi_Repos <83489424+Spurthi2021@users.noreply.github.com>
2025-01-07 17:55:44 +0100

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,41 @@
+#These are few different Dictionary Methods
+
+myDict = {
+    "Name" : "Akshay Dodwadmath",
+    "Hobbies" : ["Reading Books", "Jogging", "Thinking"],
+    "Degrees" : {
+        "BE" : "Pass",
+        "MS" : "Pass",
+        "PHD" : "Pursuing"
+    },
+    "Age" : 32
+    
+}
+print("Type :", type(myDict))
+print("Dictionary is :", myDict )
+print("Dictionary Keys : ", myDict.keys())
+print("List of Dict Keys : ", list(myDict.keys()))
+print("List of Dict Values : ", list(myDict.values()))
+print("Tuple Pairs : ", myDict.items())
+print("Tuple Pairs : ", list(myDict.items()))
+
+print(myDict["Name"])
+print(myDict.get("Name"))
+
+#print(myDict["Test"]) # This gives error as there is no Test attribute
+print(myDict.get("Test")) 
+
+
+# Update Dictionary
+Dict2 = {
+    "Test" : "Pass",
+    "Sub" : "Python"
+}
+myDict.update(Dict2)
+print(myDict)
+
+Dict3 = {
+    "Name" : "Akshay D"
+}
+myDict.update(Dict3)
+print(myDict)
