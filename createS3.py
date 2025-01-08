77a91ea8abc43346261a1e88fc3fbc8e4346cb7f
Spurthi_Repos <83489424+Spurthi2021@users.noreply.github.com>
2025-01-08 16:05:38 +0100

    Add files via upload

---
+++ createS3.py
@@ -0,0 +1,30 @@
+#Program to create S3 buckets
+
+from botocore.exceptions import ClientError
+import boto3
+
+def create_s3_bucket(bucket_name, region):
+    try:
+# Set up Ec2 client
+        s3_client = boto3.client('s3', region_name=region)  # Replace 'us-east-1' with your desired region
+
+#If Region is none
+
+        if(region is None):
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            Location = {'LocationConstraint' : region}
+            s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=Location)
+
+    
+        print(f"Bucket '{bucket_name}")
+        return True
+    
+    except ClientError as e:
+        print(f"Error: {e}")
+        return False
+        
+
+
+
+create_s3_bucket("my-unique-bucket-01-08-2025","eu-north-1")
