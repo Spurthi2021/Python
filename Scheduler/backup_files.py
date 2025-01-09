878021eb606f4719e269c7b01c84c516c9186955
Spurthi_Repos <83489424+Spurthi2021@users.noreply.github.com>
2025-01-09 23:02:49 +0100

    Update backup_files.py

--- Scheduler/backup_files.py
+++ Scheduler/backup_files.py
@@ -7,8 +7,8 @@ from datetime import datetime
 def backup_files():
     print(f"[{datetime.now()}] Backing up files...")
     # Example backup logic
-    source_dir = "C:/Users/spuhi/Python/Source/Daily_reports"  
-    backup_dir = "C:/Users/spuhi/Python/backUp"  
+    source_dir = "../Python/Scheduler/Source/Daily_reports"  
+    backup_dir = "../Python/Scheduler/backUp"  
     if not os.path.exists(backup_dir):
         os.makedirs(backup_dir)
     for filename in os.listdir(source_dir):
