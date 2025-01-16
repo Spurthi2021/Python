42ebc8eaf3f659c9609523d18c63d30c4f30515e
Spurthi_Repos <83489424+Spurthi2021@users.noreply.github.com>
2025-01-16 18:47:02 +0100

    Update automaticIssueAssignment.py

--- PythontoJira/automaticIssueAssignment.py
+++ PythontoJira/automaticIssueAssignment.py
@@ -1,3 +1,5 @@
+#Python script to automate the assignment of Jira issues to the appropriate developers or teams.
+
 import requests
 from requests.auth import HTTPBasicAuth
 import json
@@ -7,7 +9,6 @@ JIRA_URL = https://spubmath.atlassian.net/rest/api/3/issue
 API_TOKEN = {secrets.API_TOKEN}
 EMAIL = {secrets.EMAIL}
 auth = HTTPBasicAuth(EMAIL, API_TOKEN)
-
 
 assignment_rules = {
     "frontend": "accountId_frontend",
@@ -36,7 +37,6 @@ def assign_issue(issue_key, component):
         print(f"Issue {issue_key} successfully assigned to {assignee}.")
     else:
         print(f"Failed to assign issue {issue_key}. Response: {response.text}")
-
 
 issue_key = "SCRUM-23"
 component = "backend"
